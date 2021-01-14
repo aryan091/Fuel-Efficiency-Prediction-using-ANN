5342011e34ea877683d84ea040a5aa655e72cf1c
Ashish <therobomarket@gmail.com>
2021-01-14 20:59:51 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, url_for, request,jsonify
+import joblib
+import os
+import numpy as np
+import pickle
+from tensorflow.keras.models import load_model
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('home.html')
+
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+
+    cylinders=int(request.form["cylinders"])
+    displacement = int(request.form["displacement"])
+    horsepower = int(request.form["horsepower"])
+    weight = int(request.form["weight"])
+    acceleration = int(request.form["acceleration"])
+    model_year = int(request.form["model_year"])
+    origin = int(request.form["origin"])
+
+    values=[[cylinders, displacement, horsepower, weight, acceleration , model_year, origin]]
+
+    scaler_path = os.path.join(os.path.dirname('D:/Python37/Projects/Fuel Efficiency Prediction using ANN/models/'),
+                               'scaler.pkl')
+    sc = None
+    with open(scaler_path, 'rb') as f:
+        sc = pickle.load(f)
+
+    values = sc.transform(values)
+
+    model=load_model(r"D:\Python37\Projects\Fuel Efficiency Prediction using ANN\models\model.h5")
+
+    prediction=model.predict(values)
+    prediction=int(prediction)
+
+    json_dict={
+        "prediction":prediction
+    }
+    return jsonify(json_dict)
+
+
+if __name__ == "__main__":
+    app.run(debug=True,port=1347)
